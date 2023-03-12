a6aa307367d476d4a2d969add83f94a333f9b212
Ilhom Jabborov <jabborovilhom300@gmail.com>
2023-03-12 17:26:35 +0500

    Plata uchun kod yozdim

---
+++ ardu_kod.ino
@@ -0,0 +1,39 @@
+void setup() {
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(12,1);
+  delay(100);
+  digitalWrite(12,0);
+  digitalWrite(11,1);
+  delay(100);
+  digitalWrite(11,0);
+  digitalWrite(10,1);
+  delay(100);
+  digitalWrite(10,0);
+  digitalWrite(9,1);
+  delay(100);
+  digitalWrite(9,0);
+  digitalWrite(8,1);
+  delay(100);
+  digitalWrite(8,0);
+  digitalWrite(7,1);
+  delay(100);
+  digitalWrite(7,0);
+  digitalWrite(6,1);
+  delay(100);
+  digitalWrite(6,0);
+  digitalWrite(5,1);
+  delay(100);
+  digitalWrite(5,0);
+  delay(100);
+
+}
